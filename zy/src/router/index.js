ed440cf45307b1c5b6403b79c499e74ab2bcfc19
q990724 <57699194+q990724@users.noreply.github.com>
2019-11-24 02:48:25 +0800

    微医页面-半成品

--- zy/src/router/index.js
+++ zy/src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import index from "../components/cfy/index"
 Vue.use(VueRouter)
 
 const routes = [
   // {path: '/',name: 'home',component: Home},
+  {path: '/',component: index},
 ]
 
 const router = new VueRouter({
