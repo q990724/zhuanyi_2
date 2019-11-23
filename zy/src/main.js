53b53d01b63a4a891312537f287815ccef9012b8
q990724 <57699194+q990724@users.noreply.github.com>
2019-11-23 23:54:27 +0800

    初始化脚手架

---
+++ zy/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from "axios"
+import vant from "vant"
+
+axios.defaults.baseURL = "http://127.0.0.1:5050/"
+//保存session 信息
+axios.defaults.withCredentials = true;
+// 完整引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+Vue.config.productionTip = false
+
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
