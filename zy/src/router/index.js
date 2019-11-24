868c195fb3dd0a719dceba0a951c7d15a6ca7c6e
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-24 23:31:21 +0800

    急速问诊

--- zy/src/router/index.js
+++ zy/src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-11-24 00:07:15
- * @LastEditTime: 2019-11-24 15:10:26
+ * @LastEditTime: 2019-11-24 21:40:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \zhuanyi_2\zy\src\router\index.js
@@ -10,6 +10,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import jkh from '../components/wtq/jkh'
+import jswz from '../components/wtq/jswz'
 import change from '../components/wtq/change'
 import recommand from '../components/wtq/recommand'
 import index from "../components/cfy/index"
@@ -21,6 +22,7 @@ Vue.use(VueRouter)
 
 const routes = [
   // {path: '/',name: 'home',component: Home},
+  {path: '/jswz',name: 'jswz',component: jswz},
   {path: '/jkh',name: 'jkh',component: jkh},
   {path: '/recommand',name: 'recommand',component: recommand},
   {path: '/change',name: 'change',component: change},
