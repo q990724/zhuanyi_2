20e6f47f8242bfb05cf41960070d499e68ba1c38
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-25 19:13:27 +0800

    我的

---
+++ zy/src/components/wtq/nowz.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="no-wz">
+    <div class="img">
+      <!-- 问诊 -->
+      <img src= "../../../public/images/index/急速问诊/empty2.png">
+    </div>
+    <div class="text">
+      您还没有发起过任何问诊
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  #no-wz{
+    padding-top:5rem;
+  }
+  .img{
+    width:60%;
+    margin:auto;
+  }
+  .img img{
+    width:100%;
+  }
+  .text{
+    color:#83889a;
+    font-size:1rem;
+    text-align:center;
+    margin-top:.5rem;
+  }
+</style>
