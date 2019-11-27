877f3f1bebea4c39bf24ccf2a9cf2351b4683266
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-27 15:46:55 +0800

    天气

--- zy/src/components/wtq/weather.vue
+++ zy/src/components/wtq/weather.vue
@@ -17,42 +17,44 @@
         <van-col class="today" span="12">
            <!-- 地点  先来个定位 ## -->
           <div class="t-left">
-            <h3>今天</h3>
-            <p>4/7°</p>
+            <h3>今天({{today}})</h3>
+            <p>{{ttemplow}}/{{ttemphigh}}°</p>
           </div>
           <div class="t-right">
-            <h3>多云转晴</h3>
+            <!-- <h3>{{tweather}}</h3> -->
+            
             <div class="w-img">
-              <p>良</p>
-              <img style="width:20px" src="../../../public/images/index/0.png" alt />
+              <p>{{tquality}}</p>
+              <div class="img-icon">
+                <img style="width:20px" src="../../../public/images/index/0.png" alt />
+              </div>
+              <h3>优</h3>
             </div>
           </div>
         </van-col>
         <!-- 右边 -->
         <van-col class="tomorrow" span="12">
           <div class="m-left">
-            <h3>明天</h3>
-            <p>4/7°</p>
+            <h3>明天({{tomorrow}})</h3>
+            <p>{{wtemplow}}/{{wtemphigh}}°</p>
           </div>
           <div class="m-right">
-            <h3>多云转晴</h3>
-            <p>良</p>
+            <div class="w-img">
+              <div class="img-icon">
+                <img style="width:20px" src="../../../public/images/index/0.png" alt />
+              </div>
+              <h3>优</h3>
+            </div>
           </div>
         </van-col>
       </van-row>
       <!-- 生活指数 -->
-      <van-grid class="life1">
-        <van-grid-item text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-      </van-grid>
-      <van-grid class="life2">
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-        <van-grid-item icon="photo-o" text="文字"></van-grid-item>
-      </van-grid>
+      <div class="life">
+        <van-row>
+          <van-col class="life-one" span="24" >offset: 4, span: 10</van-col>
+          <van-col class="life-two" span="24" >offset: 4, span: 10</van-col>
+        </van-row>
+      </div>
     </div>
   </div>
 </template>
@@ -63,28 +65,53 @@ export default {
       return{
          myownpos:"北京",
          today:"",
-         week:"",
-         winddirect:"",
-         windpower:"",
-         windspeed:"",
-         daily:[],//一周七天的
-         index:[],//不一定全用
+         tweather:"",
+         twinddirect:"",
+         twindpower:"",
+         twindspeed:"",
+         tdaily:[],//一周七天的
+         tindex:[],//不一定全用,
+         ttemphigh:"",
+         ttemplow:"",
+         tquality:"",
+        //  明天
+         tomorrow:"",
+         wweather:"",
+         wtemphigh:"",
+         wtemplow:"",
       }
    },
    created() {
     //会在组件创建成功之后自动调用该函数
-    // 自动在组件创建完成之后发送ajax请求
-   $.ajax({
-      url : "https://api.jisuapi.com/weather/query?appkey=90b47e2a6f6c02d3&city=北京",
-      type : "GET",
-      dataType : "jsonp",
-      success : res=>{
-         console.log("发送天气请求成功");
-         console.log(res.result);
-         
-      }
+    // 自动在组件创建完成之后发送ajax请求--axios不支持jsonp
+  //  $.ajax({
+  //     url : "https://api.jisuapi.com/weather/query?appkey=90b47e2a6f6c02d3&city=北京",
+  //     type : "GET",
+  //     dataType : "jsonp",
+  //     success : res=>{
+  //        console.log("发送天气请求成功");
+  //       //  console.log(res.result);
+  //        var result = res.result;
+  //        console.log(result);
+  //        this.today = result.week;
+  //        this.tweather = result.tweather;
+  //        this.tquality = result.tquality;
+  //        this.ttemphigh = result.temphigh;
+  //        this.ttemplow = result.templow;
+  //        this.winddirect = result.winddirect;
+  //        this.windpower = result.windpower;
+  //        this.windspeed = result.windspeed;
+  //        this.daily = result.daily;
+  //        this.index = result.index;
+  //       //  明天
+  //       this.tomorrow = result.daily[1].week;
+  //       this.wweather = result.daily[1].day.weather;
+  //       this.wtemphigh = result.daily[1].day.temphigh;
+  //       this.wtemplow = result.daily[1].night.templow;
+        
+  //     }
 
-   });
+  //  });
 
 
 
@@ -122,9 +149,10 @@ export default {
 .weather-area {
   width: 95%;
   margin: auto;
-  height: 16rem;
+  height: 15rem;
   padding: 0.625rem;
   border-radius: 0.625rem;
+  background-color:#ccc;
 }
 .today,
 .tomorrow {
@@ -132,7 +160,8 @@ export default {
   height: 4rem;
   background-color: rgba(0, 0, 0, 0.1);
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding:0 .625rem;
   line-height: 2rem;
   border-radius: 0.625rem;
 }
@@ -143,9 +172,20 @@ export default {
   background-color: rgba(0, 0, 0, 0.1);
 }
 .w-img {
-  height: 1.875rem;
+  height: 4rem;
+  display:flex;
+  align-items: center;
 }
 .w-img img {
   width: 100% !important;
+}
+.img-icon{
+  width:3.4375rem;
+}
+.life-one,.life-two{
+  background-color:rgba(0, 0, 0, 0.1);
+  height:4.5rem;
+  margin-top:.625rem;
+  border-radius:.625rem;
 }
 </style>
